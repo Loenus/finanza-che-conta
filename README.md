b5f9c417e96db6359154a98a063a0f5f7a2c29e2
lorenzo <56167164+Loenus@users.noreply.github.com>
2023-08-02 22:45:25 +0200

    Refactoring

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
+### Link utili
+
 [Python-telegram-bot documentation](https://github.com/python-telegram-bot/python-telegram-bot/wiki) <br>
-[Job Queue documentation](https://docs.python-telegram-bot.org/en/stable/telegram.ext.jobqueue.html)
+[Job Queue documentation](https://docs.python-telegram-bot.org/en/stable/telegram.ext.jobqueue.html) <br>
+[Markdown Extended](https://www.markdownguide.org/extended-syntax/#fenced-code-blocks)
 
-TODO:
-- add timezone (tramite env per privacy magari)
-- aggiungendo una richiesta direttamente all'articolo, potrei fare in modo di fare uscire anche la pubblicazione ufficiale (non provvisoria) sapendo esattamente il giorno in cui uscirà (a meno di ritardi??? gestire l'errore nel caso)
-- aggiungere https://www.ecb.europa.eu/stats/financial_markets_and_interest_rates/euro_short-term_rate/html/index.en.html (giornalmente)
+<hr>
 
+### TODO
+
+- [ ] aggiungendo una richiesta direttamente all'articolo, potrei fare in modo di fare uscire anche la pubblicazione ufficiale (non provvisoria) sapendo esattamente il giorno in cui uscirà (a meno di ritardi??? gestire l'errore nel caso)
+- [ ] aggiungere https://www.ecb.europa.eu/stats/financial_markets_and_interest_rates/euro_short-term_rate/html/index.en.html (giornalmente)
+- [ ] add timezone
+
+<hr>
 
 ### how to commit (for Docker Hub Image)
 
-prima fare la commit normale,
-poi creare il tag (git tag v*.*.*) da pushare successivamente (git push origin v*.*.*)
-Inserire sempre X.Y.Z in cui
-<code>major or X</code> can be incremented if there are major changes in software, like backward-incompatible API release.
-<code>minor or Y</code> is incremented if backward compatible APIs are introduced.
-<code>patch or Z</code> is incremented after a bug fix.
+prima fare la commit normale, poi creare il tag (git tag vX.Y.Z) da pushare successivamente (git push origin vX.Y.Z). <br>
+Inserire sempre X.Y.Z, in cui
+- `major or X` can be incremented if there are major changes in software, like backward-incompatible API release.
+- `minor or Y` is incremented if backward compatible APIs are introduced.
+- `patch or Z` is incremented after a bug fix.
--- main.py
+++ main.py
@@ -21,32 +21,25 @@ job_queue = application.job_queue
 def retrieve_inflation(last_month_date): 
     URL = "https://www.istat.it/it/prezzi"
     response = requests.get(URL)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        news_list = soup.find("div", {"id": "main-content"})
-        if news_list:
-            title_to_find = "Prezzi al consumo (provvisori) - " + last_month_date
-            logging.info(title_to_find)
-            article = news_list.find("a", {"title": title_to_find}) # find( lambda tag: tag.name == "a" and "VALORE_TITLE_SPECIFICO" in tag.get("title", "") )
-            if article:
-                url_article = article['href']
-                inflation_text = article.text
-                logging.info(f"Link trovato: {url_article}")
-                logging.info(f"Cosa dice: {inflation_text}")
-
-                # estrapolo solo l'inflazione percentuale
-                percent_pattern = r"[-+]?\d+,\d+%"
-                inflations = re.findall(percent_pattern, inflation_text)
-                return inflations
-            else:
-                logging.warning(f"Nessun elemento a trovato con il valore title '{title_to_find}'")
-                return f"Nessun elemento a trovato con il valore title '{title_to_find}'"
-        else:
-            logging.warning("Nessun elemento div trovato con ID 'main-content'")
-            return "Nessun elemento div trovato con ID 'main-content'"
-    else:
-        logging.error("Errore nella richiesta HTTP")
+    if response.status_code != 200:
+        logging.error(f"Errore nella richiesta HTTP [{response.status_code}]")
         return "Errore nella richiesta HTTP"
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    title_to_find = "Prezzi al consumo (provvisori) - " + last_month_date
+    logging.info(title_to_find)
+    article = soup.find("a", {"title": title_to_find})
+    if not article:
+        logging.warning(f"Nessun elemento a trovato con il valore title '{title_to_find}'")
+        return f"Nessun elemento a trovato con il valore title '{title_to_find}'"
+
+    url_article = article['href']
+    inflation_text = article.text
+    logging.info(f"Link trovato: {url_article}")
+    logging.info(f"Cosa dice: {inflation_text}")
+    percent_pattern = r"[-+]?\d+,\d+%"
+    inflations = re.findall(percent_pattern, inflation_text)
+    return inflations
 
 async def callback_month(context: ContextTypes.DEFAULT_TYPE):
     current_date = datetime.datetime.now()
@@ -66,7 +59,7 @@ Inflazione nell'ultimo anno: {inflations[1]}"""
     CHANNEL_ID = os.environ.get('CHANNEL_ID')
     await context.bot.send_message(chat_id=CHANNEL_ID, text=message)
 
-data_check = datetime.time(17,58)
+data_check = datetime.time(20,36)
 job_month = job_queue.run_monthly(callback_month, data_check, 2)
 
 application.run_polling()
